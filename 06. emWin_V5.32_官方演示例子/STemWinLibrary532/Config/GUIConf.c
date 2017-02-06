efd046379314d0ba7fff3f10efcaa4a00d7d3029
flyleaf91 <flyleaf91@163.com>
2017-02-06 17:27:59 +0800

    修改宏配置

--- 06. emWin_V5.32_官方演示例子/STemWinLibrary532/Config/GUIConf.c
+++ 06. emWin_V5.32_官方演示例子/STemWinLibrary532/Config/GUIConf.c
@@ -63,7 +63,7 @@ Purpose     : Display controller initialization
 //
 // Define the available number of bytes available for the GUI
 //
-#define GUI_NUMBYTES  1024*1024								//modify by fire 原 0x200000
+#define GUI_NUMBYTES  (1024*1024)								//modify by fire 原 0x200000
 
 #define GUI_BLOCKSIZE 0x80
 /*********************************************************************
