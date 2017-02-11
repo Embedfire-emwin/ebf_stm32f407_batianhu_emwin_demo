974f80b876df72c355405921200cc4441100b7f1
flyleaf91 <flyleaf91@163.com>
2017-02-11 14:59:47 +0800

    增加ucos XBF字体移植例程

---
+++ 17.ucos_emwin使用XBF字体/User/APP/FatFsApp.c
@@ -0,0 +1,168 @@
+/**
+  ******************************************************************************
+  * @file    GUIDEMO_Fatfs.c
+  * @author  fire
+  * @version V1.0
+  * @date    2013-xx-xx
+  * @brief   测试文件系统
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:秉火  STM32 F407 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************************
+  */ 
+
+#include "includes.h"
+
+
+
+FIL fnew;													/* file objects */
+FATFS fs;													/* Work area (file system object) for logical drives */
+FRESULT res; 
+UINT br, bw;            					/* File R/W count */
+BYTE buffer[4096]={0};       		  /* file copy buffer */
+char utf8_buffer[4096*2]={0};       		  /* file copy buffer */
+BYTE textFileBuffer[] = "欢迎使用秉火F407开发板，这是一个使用XBF字体显示的例子，本例子支持中文显示。";
+
+//要测试的磁盘号，0为spi，1为sd卡
+char disk_num[] = "0:";
+
+
+
+void Fatfs_MainTask(void)
+{
+		char Str_temp[100];
+	  OS_ERR      os_err;
+
+		 
+		/* 用于显示文件内容的窗口大小 */ 
+		GUI_RECT file_content;
+	
+		//选择字体
+		GUI_SetFont(&FONT_XINSONGTI_19);
+	
+		file_content.x0 = 5;
+		file_content.y0 =GUI_GetFontSizeY() * 7;
+		file_content.x1 = LCD_GetXSize()-5;
+		file_content.y1 = LCD_GetYSize()-5;
+	
+		/* 给屏幕刷上背景颜色 */
+		GUI_Clear();	
+	  
+		//文字转换成UTF8格式
+		COM_gbk2utf8("挂载文件系统...",utf8_buffer);
+
+		GUI_DispStringAt (utf8_buffer,5,GUI_GetFontSizeY()*1);
+		
+		GUI_Delay(500);		
+		
+		/* Register work area for each volume (Always succeeds regardless of disk status) */
+		res = f_mount(&fs,disk_num,1);	
+		
+		if(res == FR_NO_FILESYSTEM)
+		{
+			/* 设置前景颜色（字体颜色）*/
+			GUI_SetColor(GUI_RED);
+
+			//文字转换成UTF8格式
+			COM_gbk2utf8("存储器没有文件系统，需要格式化！",utf8_buffer);
+
+			GUI_DispStringAt (utf8_buffer,5,GUI_GetFontSizeY()*3);
+			while(1);
+		}
+		else if	(res != FR_OK)
+		{
+			/* 设置前景颜色（字体颜色）*/
+			GUI_SetColor(GUI_RED);
+			
+			sprintf(Str_temp,"文件系统 错误:%d !",res);
+			
+			//文字转换成UTF8格式
+			COM_gbk2utf8(Str_temp,utf8_buffer);
+
+			GUI_DispStringAt (utf8_buffer,5,GUI_GetFontSizeY()*3);
+			while(1);
+		}
+
+		/* function disk_initialize() has been called in f_open */
+		
+	  GUI_DispStringAt ("f_open :newfile.txt ...",5,GUI_GetFontSizeY()*2);
+	
+		//拼接文件路径
+		sprintf(Str_temp,"%snewfile.txt",disk_num);
+		/* Create new file on the drive  */
+		res = f_open(&fnew, Str_temp, FA_CREATE_ALWAYS | FA_WRITE );
+ 
+		if ( res == FR_OK )
+		{
+			res = f_write(&fnew, textFileBuffer, sizeof(textFileBuffer), &bw);
+			f_close(&fnew);      
+		}
+		else
+		{
+			/* 设置前景颜色（字体颜色）*/
+			GUI_SetColor(GUI_RED);
+			
+			sprintf(Str_temp,"文件系统 错误:%d !",res);
+			
+			//文字转换成UTF8格式
+			COM_gbk2utf8(Str_temp,utf8_buffer);
+
+			GUI_DispStringAt (utf8_buffer,5,GUI_GetFontSizeY()*5);
+			while(1);
+		}
+
+		GUI_DispStringAt ("f_read :newfile.txt ...",5,GUI_GetFontSizeY()*3);
+		
+		res = f_open(&fnew, Str_temp, FA_OPEN_EXISTING | FA_READ); 	 
+		res = f_read(&fnew, buffer, sizeof(buffer), &br); 
+
+		printf("\r\n %s ", buffer);
+				
+		//文字转换成UTF8格式
+		COM_gbk2utf8("文件内容：",utf8_buffer);
+
+		GUI_DispStringAt (utf8_buffer,5,GUI_GetFontSizeY()*5);		
+		
+		/* 设置前景颜色（字体颜色）*/
+		GUI_SetColor(GUI_RED);
+		
+		/* 设置字体 */
+		GUI_SetFont(&FONT_XINSONGTI_25);
+		
+		//把读取到的文字转换成UTF8格式
+		COM_gbk2utf8((char*)buffer,utf8_buffer);
+
+		/* 显示文本到屏幕上 */
+		GUI_DispStringInRectWrap((const char *)utf8_buffer,&file_content,GUI_TA_LEFT,GUI_WRAPMODE_WORD) ;
+		
+		/* Close open files */
+		f_close(&fnew);	                                      
+		 
+    /* Unregister work area prior to discard it */
+		f_mount(NULL,disk_num,1);	
+		
+		
+				//把读取到的文字转换成UTF8格式
+		COM_gbk2utf8((char*)buffer,(char*)utf8_buffer);
+
+		/* 显示文本到屏幕上 */
+		GUI_DispStringInRectWrap((const char *)utf8_buffer,&file_content,GUI_TA_LEFT,GUI_WRAPMODE_WORD) ;
+
+		
+		while(1)
+		{
+				  //延时，所有任务函数的死循环内都应有至少1ms延时
+		  //特别是高优先级的任务，若无延时，其它低优先级任务可能会无机会执行
+			OSTimeDly(1000u, 
+									OS_OPT_TIME_DLY,
+									&os_err);
+
+		}
+}
+
+/*********************************************END OF FILE**********************/
+
---
+++ 17.ucos_emwin使用XBF字体/User/APP/GUIBaseTest.c
@@ -0,0 +1,76 @@
+/**
+  ******************************************************************************
+  * @file    GUIBaseTest.c
+  * @author  fire
+  * @version V1.0
+  * @date    2015-xx-xx
+  * @brief   emwin基础显示例程
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:秉火  STM32 F407 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************************
+  */
+  
+#include <includes.h>
+
+
+/**
+  * @brief  GUIBase_Test，任务函数，emwin基础显示测试，
+  * @param  无  
+  * @retval 无
+  */
+void GUIBase_Test ( void )
+{	
+	int i=0;
+	char str_temp[50];
+	OS_ERR      os_err;
+
+
+	//清除屏幕
+	GUI_Clear();
+
+	printf("\r\n **********  emwin基础显示例程*********** \r\n"); 
+	printf("\r\n 本程序不支持中文 \r\n"); 
+
+	GUI_SetColor(GUI_WHITE);   
+	/* 显示测试 */
+	GUI_DispStringAt("wildfire F407 emWin test!",10,10);
+	GUI_Delay(100);
+	
+	while (1)
+	{
+			//显示数字
+			GUI_DispDecAt(i++,50,50,4);
+			if(i>9999)
+				i=0; 
+			
+			//显示CPU使用率
+			sprintf(str_temp,"cpu usage:%.2f%%",((float)OSStatTaskCPUUsage/100));
+			GUI_DispStringAtCEOL(str_temp,10,100);
+
+		
+			//延时，所有任务函数的死循环内都应有至少1ms延时
+		  //特别是高优先级的任务，若无延时，其它低优先级任务可能会无机会执行
+			
+			//使用GUI_Delay是等效的
+			
+			//可以修改延时值，查看CPU使用率的差异
+			OSTimeDly(1000u, 
+									OS_OPT_TIME_DLY,
+									&os_err);
+
+
+	}
+	
+}
+
+
+
+
+
+/*********************************************END OF FILE**********************/
+
